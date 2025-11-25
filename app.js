6bad5f24e618d53b09ee8fcc9ed20b37f303d750
Martins Zeltins <queeeeenz@gmail.com>
2025-11-25 08:59:35 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById('screen')
+const canvasCtx = canvas.getContext('2d')
+
+let mouseX = 0
+let mouseY = 0
+let lastMouseX = 0
+let lastMouseY = 0
+
+const windows = []
+
+canvas.addEventListener('mousemove', (event) => {
+    const rect = canvas.getBoundingClientRect()
+    mouseX = event.clientX - rect.left
+    mouseY = event.clientY - rect.top
+})
+
+const drawCursor = () => {
+    canvasCtx.fillStyle = 'black'
+    canvasCtx.strokeStyle = 'black'
+    canvasCtx.lineWidth = 1
+    
+    canvasCtx.beginPath()
+    canvasCtx.moveTo(mouseX, mouseY)
+    canvasCtx.lineTo(mouseX, mouseY + 18)
+    canvasCtx.lineTo(mouseX + 5, mouseY + 14)
+    canvasCtx.lineTo(mouseX + 8, mouseY + 20)
+    canvasCtx.lineTo(mouseX + 10, mouseY + 19)
+    canvasCtx.lineTo(mouseX + 7, mouseY + 13)
+    canvasCtx.lineTo(mouseX + 12, mouseY + 13)
+    canvasCtx.closePath()
+    canvasCtx.fill()
+    canvasCtx.stroke()
+}
+
+const createWindow = (x, y, width, height) => {
+    const window = { x, y, width, height }
+    windows.push(window)
+
+    return window
+}
+
+const drawWindows = () => {
+    for (const window of windows) {
+        canvasCtx.fillStyle = 'white'
+        canvasCtx.fillRect(window.x, window.y, window.width, window.height)
+        canvasCtx.strokeStyle = 'black'
+        canvasCtx.strokeRect(window.x, window.y, window.width, window.height)
+    }
+}
+
+const render = () => {
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height)
+    drawWindows()
+    drawCursor()
+
+    lastMouseX = mouseX
+    lastMouseY = mouseY
+
+    requestAnimationFrame(render)
+}
+
+render()
